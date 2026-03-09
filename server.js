76a93c6046a556299c1d50aeb5e42727ff72b012
Muhammad <mzohaib@euthyna.ai>
2026-03-09 20:35:12 +0500

    updated

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ if (!fs.existsSync(uploadDir)) {
 const app = express();
 
 app.use(cors({
-  origin: ['*'],
-  // origin: ['https://sqa-task-frontend.vercel.app', 'http://localhost:3000', 'http://localhost:5173'],
+  // origin: ['*'],
+  origin: ['https://sqa-task-re3lvzfsi-zealouszohaib1s-projects.vercel.app/', 'http://localhost:3000', 'http://localhost:5173'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
